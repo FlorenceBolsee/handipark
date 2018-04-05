7ffe80234609de7485efa16b58222c82ece8ed09
FlorenceBolsee <florence.bolsee@gmail.com>
2018-04-05 19:09:26 +0200

    multiple markers

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,64 @@
+var roundMarker = {
+  path: 'M-10,0a10,10 0 1,0 20,0a10,10 0 1,0 -20,0',
+  fillColor: 'tomato',
+  fillOpacity: 1.0, 
+  scale: 2,
+  strokeWeight: 0
+}
+
+var markerPos = {
+  lat: 50.8469571,
+  lng: 4.4582605
+}
+
+var marker;
+
+var map;
+
 function initMap() {
     var center = {
       lat: 50.8469571,
       lng: 4.4582605
     };
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
       zoom: 12,
       center: center,
       gestureHandling: 'cooperative',
     });
-    var iconBase = '../img/pin.png';
-    var marker = new google.maps.Marker({
-      position: center,
-      map: map,
-      icon: iconBase
+    var infowindow = new google.maps.InfoWindow();
+}
+
+function createMarker(latlng, name){
+  marker = new google.maps.Marker({
+    position: latlng,
+    map: map,
+    icon: name
+  });
+}
+
+var markersList = [];
+
+var markersAjaxCall = $.getJSON("https://handikapapp.azurewebsites.net/api/RData", function() {
+  console.log( "success" );
+})
+  .done(function(data) {
+    markersList = data;
+    initMap();
+    $.each(markersList, function(){
+      if(this.Available){
+        roundMarker.fillColor = 'limegreen';
+      }
+      markerPos.lat = parseFloat(this.Latitude);
+      markerPos.lng = parseFloat(this.Longitude);
+
+      createMarker(markerPos, roundMarker);
     });
-    marker.setAnimation(google.maps.Animation.DROP);
-  }
+  })
+  .fail(function() {
+    console.log( "fail" );
+  })
+  .always(function() {
+    console.log( "complete" );
+  });
+
+
